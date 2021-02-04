7566bf32c2688c713f25d49669511e2db608b3f3
Iago Leal <iago.lealf@gmail.com>
2021-02-03 23:46:36 -0300

    Use Seq for adding elements on the end of the keyword stack

--- src/Main.hs
+++ src/Main.hs
@@ -1,15 +1,20 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import qualified Data.Text as T
-import qualified Data.Map.Strict  as M
-import Data.List (find)
+import qualified Data.Text       as T
+import qualified Data.Map.Strict as M
+import qualified Data.Sequence   as S
+import           Data.List       (find)
+import           Data.Foldable   (toList)
 
 type Token = T.Text
-type DecompRules = (T.Text, T.Text)
+
+data DecompRule = Rule T.Text [T.Text]
+  deriving Show
+
 data Keyword = Keyword { kwToken :: Token
                        , kwPrecedence :: Int
-                       , kwRules :: [DecompRules]
+                       , kwRules :: [DecompRule]
                        }
   deriving Show
 
@@ -19,9 +24,9 @@ reflections = M.fromList
   [("am", "are"),
    ("was", "were"),
    ("i", "you"),
-   ("i’d", "you would"),
-   ("i’ve", "you have"),
-   ("i’ll", "you will"),
+   ("i'd", "you would"),
+   ("i've", "you have"),
+   ("i'll", "you will"),
    ("my", "your"),
    ("are", "am"),
    ("you’ve", "I have"),
@@ -33,12 +38,12 @@ reflections = M.fromList
 
 keywords :: [Keyword]
 keywords = [ Keyword "happy" 5 []
-           , Keyword "me"   4 []
-           , Keyword "sad" 10 []
+           , Keyword "me"    4 []
+           , Keyword "sad"  10 []
            ]
 
 tokenize :: T.Text -> [Token]
-tokenize = T.words
+tokenize = T.words . T.toLower
 
 reflect :: [Token] -> [Token]
 reflect = fmap reflectToken
@@ -48,21 +53,20 @@ reflect = fmap reflectToken
      Just x  -> x
 
 scanKeywords :: [Token] -> [Keyword]
-scanKeywords l = loop l [] (-1)
+scanKeywords l = toList $ loop l S.Empty (-1)
  where
   loop []     stack _ = stack
   loop (w:ws) stack p =
     if w == "." || w == ","
-      then if null stack
-            then loop ws stack p
-            else stack
-      else case find ((==w) . kwToken) keywords of
-        Nothing -> loop ws stack p
-        Just k  -> if (kwPrecedence k) > p
-                    then loop ws (k:stack) (kwPrecedence k)
-                    -- Should use a Seq here
-                    else loop ws (stack ++ [k]) p
-
+     then if null stack
+           then loop ws stack p
+           else stack
+     else case find ((==w) . kwToken) keywords of
+       Nothing -> loop ws stack p
+       Just kw -> if (kwPrecedence kw) > p
+                   then loop ws (kw S.:<| stack) (kwPrecedence kw)
+                   -- Should use a Seq here
+                   else loop ws (stack S.:|> kw) p
 
 
 match :: [Keyword] -> [Token] -> T.Text
