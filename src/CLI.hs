d035c9a320c781f782a52880e9bf53dcd9378928
Iago Leal <iago.lealf@gmail.com>
2021-03-10 20:36:23 -0300

    More polished interface using mtl

--- src/CLI.hs
+++ src/CLI.hs
@@ -1,34 +1,47 @@
-module CLI (cliRepl) where
+module CLI where
 
 import qualified Data.Text       as T
 import qualified Data.Text.IO    as T
 
 import Control.Monad.State
-import Control.Monad
 import System.IO
+
+import qualified Text.Megaparsec as MP
+import qualified Text.Megaparsec.Char as MP
 
 import Eliza
 
-data UserInput = CmdQuit | CmdError | Input T.Text
+data UserInput = CmdQuit | CmdError | CmdHelp | CmdLoad FilePath | Input T.Text
+  deriving Show
+
+hoistState :: State s a -> StateT s IO a
+hoistState = state . runState
 
 cliRepl :: Script -> IO ()
 cliRepl script = do
-  bot <- initBotState script
-  let (greet, bot') = runState (pickAny (greetings . botScript $ bot)) bot
-  cliOutput greet
-  repl bot
+    T.putStrLn initialMsg
+    bot <- initBotState script
+    flip evalStateT bot $ do
+      greets <- greetings . botScript <$> get
+      greet  <- hoistState (pickAny greets)
+      lift (cliOutput greet)
+      repl
 
-repl bot = do
-  input <- cliInput
+repl :: StateT BotState IO ()
+repl = do
+  input <- lift cliInput
   case processInput input of
-    CmdQuit  ->
-      cliOutput "Goodbye"
-    CmdError ->
-      cmdErrorMsg input >> repl bot
+    CmdQuit  -> lift (cliOutput "Goodbye")
+    CmdError -> lift (cmdErrorMsg input) >> repl
+    CmdHelp  -> lift (T.putStrLn helpMsg) >> repl
+    CmdLoad file -> do
+      script <- lift $ loadScript file
+      modify (\bot -> bot{botScript = script})
+      repl
     Input t  -> do
-      let (response, s) = runState (answer input) bot
-      cliOutput response
-      repl s
+      response <- hoistState (answer t)
+      lift (cliOutput response)
+      repl
 
 botPrompt :: T.Text
 botPrompt = "eliza> "
@@ -36,6 +49,21 @@ botPrompt = "eliza> "
 userPrompt :: T.Text
 userPrompt = "> "
 
+initialMsg :: T.Text
+initialMsg =
+  "Welcome to ELIZA!\n\
+  \Type your questions and press 'Enter'\n\
+  \For more info, type ':help'\n\n"
+
+helpMsg :: T.Text
+helpMsg =
+  "Available commands:\n\n\
+  \  :help\t    show this message\n\
+  \  :load\t    load new script\n\
+  \  :quit\t    exit Eliza\n\
+  \\n"
+
+cliInput :: IO T.Text
 cliInput = do
   T.putStr userPrompt
   hFlush stdout
@@ -43,12 +71,23 @@ cliInput = do
   T.putStrLn ""
   pure input
 
+cliOutput :: T.Text -> IO ()
 cliOutput out = T.putStrLn (botPrompt <> out <> "\n")
 
-processInput input
-  | elem input [":quit", ":q", ":bye"] = CmdQuit
-  | not (T.null input) && T.head input == ':' = CmdError
-  | otherwise = Input input
+processInput :: T.Text -> UserInput
+processInput s = maybe (Input s) id $ MP.parseMaybe commands s
+  where
+   cmdQuit  = CmdQuit  <$  parseCmd ["quit", "q", "bye"]
+   cmdHelp  = CmdHelp  <$  parseCmd ["help", "h"]
+   cmdLoad  = CmdLoad  <$> (parseCmd ["load", "l"] >> MP.some MP.anySingle)
+   cmdError = CmdError <$ (MP.char ':' >> MP.many MP.anySingle)
+   commands = MP.choice $ fmap MP.try [cmdHelp, cmdQuit, cmdLoad, cmdError]
 
-cmdErrorMsg input =
-  T.putStrLn $ "Sorry, command non-recognized: " <> input
+parseCmd :: (Foldable f, Functor f) => f T.Text -> Parser T.Text
+parseCmd xs = MP.char ':' *> (MP.choice $ fmap exactWord xs)
+
+safeTextHead :: T.Text -> Maybe Char
+safeTextHead = fmap fst . T.uncons
+
+cmdErrorMsg :: T.Text -> IO ()
+cmdErrorMsg input = T.putStrLn $ "Sorry, non-recognized command: " <> input
