5a7fa45fcf7a88df9c5f228f8af291a137c17012
iago <iago.lealf@gmail.com>
2021-03-26 21:59:35 -0300

    Better exception treatment when reading files on CLI

--- app/Main.hs
+++ app/Main.hs
@@ -1,31 +1,32 @@
 module Main (main) where
 
+import Control.Monad
+import Control.Exception
+
 import System.Console.GetOpt
 import System.Environment
 import System.Exit
 import System.IO
+import System.IO.Error
+
+import Text.Read (readMaybe)
 
 import CLI
 import Eliza
-
--- | The configuration necessary to run a Eliza program
-data Config = Config
-  { cfgTypingSpeed :: Int     -- ^ On average, how many words the bot types per second.
-  , cfgScript      :: Script  -- ^ Where the script file is located.
-  }
 
 -- | The default configuration.
 -- Used if the program receives no flag or argument.
 defaultOptions :: Config
 defaultOptions = Config { cfgTypingSpeed = 80
-                        , cfgScript = defaultScript
+                        , cfgScript      = defaultScript
                         }
 
 main :: IO ()
 main = do
   args <- getArgs
   (cfg, errors)  <- parseArguments args
-  print $ errors
+  when (not (null errors))
+    (print errors)
   cliRepl (cfgScript cfg)
 
 -- | Get a list of command line arguments and return a configuration and possible errors.
@@ -36,31 +37,42 @@ parseArguments args = do
   cfg <- case nonOptions of
     []        -> pure opts
     (fname:_) -> do
-      script <- loadScript fname
+      script <- scriptFromFile fname
       pure $ opts {cfgScript = script}
   pure (cfg, errors)
 
+scriptFromFile :: FilePath -> IO Script
+scriptFromFile fname = do
+  catchJust (\e -> if isDoesNotExistErrorType (ioeGetErrorType e) then Just () else Nothing)
+            (loadScript fname)
+            (\_ -> do
+              prog <- getProgName
+              hPutStrLn stderr (prog <> ": No such file or directory -- " <> show fname)
+              exitWith (ExitFailure 1))
 -- | Command line options to parse with 'System.Console.GetOpt'.
 options :: [OptDescr (Config -> IO Config)]
 options =
   [ Option [] ["wps", "words-per-second"]
     (ReqArg
-      (\arg opt -> pure opt {cfgTypingSpeed = read arg})
+      (\arg opt -> case (readMaybe arg :: Maybe Int) of
+        Nothing -> do
+          prog <- getProgName
+          hPutStrLn stderr (prog <> ": invalid --wps argument \'" <> arg <> "\'\nShould be an integer.")
+          exitWith (ExitFailure 1)
+        Just n  -> pure opt{cfgTypingSpeed = n})
       "<num>")
     "How fast can Eliza type?"
   , Option "v" ["version"]
     (NoArg (\_ -> do
       prog <- getProgName
       hPutStrLn stderr (prog <> " version 0.1.0")
-      exitWith ExitSuccess)
-    )
+      exitWith ExitSuccess))
     "Print version and exit"
   , Option "h" ["help"]
     (NoArg (\_ -> do
       prog <- getProgName
-      let header = "Usage: " <>  prog <> " [OPTIONS]... [FILE]"
+      let header = "Usage: " <> prog <> " [OPTIONS]... [FILE]"
       hPutStrLn stderr (usageInfo header options)
-      exitWith ExitSuccess)
-    )
+      exitWith ExitSuccess))
     "Show this help message and exit"
   ]
